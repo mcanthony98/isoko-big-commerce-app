9e2ae6f9eb027a28a7eb906e165ff9189c458c5f
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2020-09-06 21:35:51 +0300

    update connection

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', ' remotemysql.com');
+define('DB_SERVER', 'remotemysql.com');
 define('DB_USERNAME', 'xjEN2T9eQx');
 define('DB_PASSWORD', 'c7MM0MP2ml');
 define('DB_DATABASE', 'xjEN2T9eQx');
@@ -11,5 +11,4 @@ $pdo = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 	 if ($pdo->connect_error){
 		 die("Connection Failed: <br />" .$pdo->connect_error);
 	  }
- 
 ?>
