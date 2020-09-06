b55c103a3528a9c00d98646ee1a9afc9c861134b
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2020-09-06 21:40:56 +0300

    update mysql connection

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', 'remotemysql.com');
 define('DB_USERNAME', 'xjEN2T9eQx');
-define('DB_PASSWORD', 'c7MM0MP2mlll');
+define('DB_PASSWORD', 'c7MM0MP2ml');
 define('DB_DATABASE', 'xjEN2T9eQx');
 
 $pdo = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
